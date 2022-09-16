b37094bb42d873bab4ea04ae7cf145e8ef58e54d
Doug A <douganderson444@gmail.com>
2022-09-16 12:16:33 -0300

    add mdsvex layout

---
+++ src/layouts/handwritten.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '../app.css';
+
+	// These props get filled in from the page's front matter
+	export let title = '';
+	export let coverImageUrl = '';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta property="og:title" content={title} />
+	<meta property="og:image" content={coverImageUrl} />
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism-okaidia.css"
+	/>
+</svelte:head>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		font-family: 'Architects Daughter', cursive;
+		font-weight: normal;
+		font-style: normal;
+		font-size: large;
+	}
+</style>
