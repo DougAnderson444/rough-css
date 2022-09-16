e5ee8cd6f937831f9c9f8171db207e45e5f491fa
Doug A <douganderson444@gmail.com>
2022-09-16 12:16:58 -0300

    add circle, diamond

---
+++ src/lib/RoughCircle.svelte
@@ -0,0 +1,57 @@
+<script>
+	let min = -1;
+	let max = 2; // 1 or 2 looks like a sketch
+
+	let high90s = () => 100 - Math.random() * (max - min) + min;
+	let singlDigs = () => 1 + Math.random() * (max - min) + min;
+
+	let bordRad = `${
+		high90s() * 1.12
+	}% ${high90s()}% ${high90s()}% ${high90s()}%/${high90s()}% ${high90s()}% ${high90s()}% ${high90s()}%`;
+
+	let borderWidth = `${singlDigs() * 0}px ${singlDigs()}px ${singlDigs()}px ${singlDigs()}px`;
+
+	let rotate = () => `rotate(${Math.floor(singlDigs() / 2)}deg)`;
+</script>
+
+<div
+	class="circle"
+	style:border-radius={bordRad}
+	style:transform={rotate()}
+	style:border-width={borderWidth}
+>
+	<div class="inner" contenteditable>
+		<slot />
+	</div>
+</div>
+
+<style>
+	.circle {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 5px;
+		min-width: fit-content;
+		height: auto;
+		aspect-ratio: 1 / 1;
+		background: #fff;
+		border: solid black;
+		border-color: black;
+		float: left;
+		text-align: center;
+	}
+
+	.inner {
+		margin: 15px;
+		/* transform: rotate(-2deg); */
+	}
+	.boxinner {
+		transform: rotate(2deg);
+		margin: 15px;
+		/*   padding:0 5px; */
+		float: right;
+		background: #ddd;
+		border: 1px solid #222;
+		box-shadow: 3px 3px 0 #222;
+	}
+</style>
---
+++ src/lib/RoughDiamond.svelte
@@ -0,0 +1,52 @@
+<script>
+	let min = -1;
+	let max = 2; // 1 or 2 looks like a sketch
+
+	let high90s = () => 100 - Math.random() * (max - min) + min;
+	let singlDigs = () => 1 + Math.random() * (max - min) + min;
+
+	let bordRad = `${high90s()}% ${singlDigs()}% ${high90s()}% ${singlDigs()}%/${singlDigs()}% ${high90s()}% ${singlDigs()}% ${high90s()}%`;
+
+	let borderWidth = `${singlDigs()}px ${singlDigs()}px ${singlDigs()}px ${singlDigs()}px`;
+
+	let rVal = Math.floor(singlDigs() / 2) + 45;
+	let rotate = `rotate(${rVal}deg)`;
+
+	let mounted = false;
+</script>
+
+<div class="outter">
+	<div
+		class="box"
+		style:border-radius={bordRad}
+		style:transform={`rotate(${rVal}deg)`}
+		style:border-width={borderWidth}
+	>
+		<div contenteditable style:transform={`rotate(${-rVal}deg)`}>
+			<slot />
+		</div>
+	</div>
+</div>
+
+<style>
+	div {
+		float: left;
+	}
+	.outter {
+		display: flex;
+		flex-direction: column;
+	}
+	.box {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		aspect-ratio: 1 / 1;
+		margin: 5px;
+		min-width: fit-content;
+		background: #fff;
+		border: solid black;
+		border-color: black;
+		padding: 15px;
+	}
+</style>
